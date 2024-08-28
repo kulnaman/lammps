ad0976eadd469041901514f2df3ea4b5ff5326e9
Naman Kulshreshtha <nkulshr@clemson.edu>
2024-08-28 11:00:28 -0700

    added build script

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#Kripke does not support latest version of CUDA/12 and hence to use older version of GCC. 
-#Also need to change the host-configs/llnl-blueos-V100-nvcc-gcc.cmake
-PREFIX_PATH=$BENCHMARK_PATH/benchmarks/lammps/install_lassen
+
+hostname=$(hostname)
+echo $hostname
 set -e #Exit on error
 set -o pipefail #also consider pipe when exiting
+rm -rf ./build
 source module_file.sh
 mkdir build
 cd build
+if [[ "$hostname" == lassen* ]]; then
+PREFIX_PATH=$BENCHMARK_PATH/benchmarks/lammps/install_lassen
 cmake \
   -D CMAKE_INSTALL_PREFIX=${PREFIX_PATH} \
   -D Kokkos_ARCH_VOLTA70=ON \
@@ -19,5 +22,22 @@ cmake \
   -D PKG_KOKKOS=yes \
   -D Kokkos_ENABLE_CUDA=yes \
   ../cmake
+elif [[ "$hostname" == tioga* ]]; then
+PREFIX_PATH=$BENCHMARK_PATH/benchmarks/lammps/install_tioga
+cmake \
+  -D CMAKE_INSTALL_PREFIX=${PREFIX_PATH} \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D MPI_CXX_COMPILER=mpicxx \
+  -D CMAKE_CXX_COMPILER=hipcc \
+  -D PKG_ML-SNAP=yes \
+  -D BUILD_MPI=yes \
+  -D CMAKE_TUNE_FLAGS="-munsafe-fp-atomics" \
+  -D PKG_ML-SNAP=yes \
+  -D PKG_GPU=no \
+  -D PKG_KOKKOS=yes \
+  -D Kokkos_ENABLE_HIP_MULTIPLE_KERNEL_INSTANTIATIONS=yes \
+  -D Kokkos_ENABLE_HIP=yes \
+  ../cmake
+fi
 make -j
 make install
--- module_file.sh
+++ module_file.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+hostname=$(hostname)
+if [[ "$hostname" == lassen* ]]; then
 module load cuda/12.2
 module load gcc/12
 module load cmake/3.29
+elif [[ "$hostname" == tioga* ]]; then
+module load rocm/6.0.3 
+module load rocmcc/6.0.3-cce-17.0.1-magic
+fi
